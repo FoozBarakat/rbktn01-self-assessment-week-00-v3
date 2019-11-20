9bf1a5db671b2bf4b8d4d3caa42720c65ce57726
FoozBarakat <56113235+FoozBarakat@users.noreply.github.com>
2019-11-20 09:51:55 +0100

    self_assement

--- filter-family-members/filter-family-members.js
+++ filter-family-members/filter-family-members.js
@@ -1,3 +1,4 @@
+console.log('test');
 // `filterFamilyMembers` accepts two arguments, a family tree object, and a truth test. `filterFamilyMembers` 
 // returns an array, in any order, of the full names of family members who pass the passed in truth test.
 // You will need to use recursion in your solution in order to handle nested family trees.
@@ -13,36 +14,37 @@
 //
 // EXAMPLE:
 //
-// var familyTree = {
-//   'firstName': 'Beth',
-//   'lastName': 'Johnson',
-//   'location': 'San Francisco',
-//   'children': [
-//     {
-//       'firstName': 'Beth Jr.',
-//       'lastName': 'Johnson',
-//       'location': 'Berkeley',
-//       'children': [
-//         {
-//           'firstName': 'Smitty',
-//           'lastName': 'Won',
-//           'location': 'Beijing',
-//           'children': []
-//         }
-//       ]
-//     },
-//     {
-//       'firstName': 'Joshie',
-//       'lastName': 'Wyattson',
-//       'location': 'Berkeley',
-//       'children': []
-//     }
-//   ]
-// };
+var familyTree = {
+  'firstName': 'Beth',
+  'lastName': 'Johnson',
+  'location': 'San Francisco',
+  'children': [
+    {
+      'firstName': 'Beth Jr.',
+      'lastName': 'Johnson',
+      'location': 'Berkeley',
+      'children': [
+        {
+          'firstName': 'Smitty',
+          'lastName': 'Won',
+          //'location': 'Beijing',
+          'location': 'Berkeley',
+          'children': []
+        }
+      ]
+    },
+    {
+      'firstName': 'Joshie',
+      'lastName': 'Wyattson',
+      'location': 'Berkeley',
+      'children': []
+    }
+  ]
+};
 //
-// var livesInBerkeley = function (familyMember) {
-//   return familyMember.location === 'Berkeley';
-// }
+var livesInBerkeley = function (familyMember) {
+  return familyMember.location === 'Berkeley';
+}
 //
 // filterFamilyMembers(familyTree, livesInBerkeley)
 //
@@ -51,6 +53,28 @@
 
 
 var filterFamilyMembers = function (familyTree, truthTest) {
-  // All your code in this function body
+  var result = [];
+  
+  for (var key in familyTree['children']) {
+
+  	//console.log( familyTree['children'][key] )
+  	if (truthTest(familyTree['children'][key]) === true) {
+  		var name = familyTree['children'][key]['firstName'] + " " + familyTree['children'][key]['lastName'];
+  		result.push(name);
+  	}
+  	// delete familyTree['children'][key];
+  	// console.log(familyTree['children']);
+  	// console.log( familyTree['children'].length);
+  }
+
+  // if (familyTree['children'].length === 0) {
+  // 	return result;
+  // }
+
+  return result;
+
+  // return filterFamilyMembers(familyTree, livesInBerkeley);
 };
+
+console.log( filterFamilyMembers(familyTree, livesInBerkeley) );
 
